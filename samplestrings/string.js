c41543a000985d6d720f4dd0681403d4514e0753
Shilpaa12 <shilpapathlavath7@gmail.com>
2022-12-06 14:29:05 +0530

    java

---
+++ samplestrings/string.js
@@ -0,0 +1,93 @@
+// // string:
+// let firstName="Harshit";
+// let lastName="Harsh";
+// let fullName=`Harsha`;
+// let age=20;
+// //  1 .console.log(firstName,lastName,fullName);
+// console.log("my name is",fullName);
+
+// // 2.length of the string:
+// console.log(lastName.length); 
+// // or
+// let strLen =lastName.length;
+// console.log(strLen);
+
+// // 3. length concatenation:
+// let newStr ="I am "+ firstName +"  and I am "+age+"years old";
+// console.log(newStr);
+
+// // 4.template String: 
+//  let newStrs =` I am ${firstName}and I am ${age}years old`;
+//  console.log(newStr);
+
+// // 5. escape characters:
+// // let strLenn="I'm shilpa";
+// // let strLenn=`I'm shilpa`;
+// let strLenn='I\'m shilpa';
+// console.log(strLenn);
+
+// //6. String indexing:
+// let secondName ="shilpaAriqt";
+// let strng=secondName[1];
+// console.log(strng);
+// // or
+// let newStrng=secondName[secondName.length-5];
+// console.log(secondName.length);
+
+// // 7.string slice:
+// let secondNam ="shilpaAriqt";
+// let newstr=secondNam.slice(3,8);
+// // let newstr=secondNam.substring(0,5);
+// console.log(newstr);
+
+// 8.string lowercase:
+// let newName ="SHILPAPATHLAVATH";
+// let newstrng=newName.toLowerCase();
+// let newName ="shilpapathlavath";
+// let newstrng=newName.toUpperCase();
+// console.log(newstrng);
+
+// 9.string replace:
+// let newName ="shilpa pathlavath";
+// let newstrng=newName.replace("shilpa","roopa");
+// console.log(newstrng);
+// or
+
+// let text = "I love cats. Cats are very easy to love. Cats are very popular."
+// text = text.replaceAll("Cats","Dogs");
+// text = text.replaceAll("cats","dogs");
+// document.getElementById("demo").innerHTML = text;
+
+//  Arrays
+//  const cars=["car","car1","car2"];
+//  cars[2]="volvo";
+//  document.getElementById("demo").innerHTML=cars;
+// const fruits=["mango","apple","bannana"];
+// document.getElementById("demo1").innerHTML=fruits;
+// fruits.push("kiwi");
+// document.getElementById("demo2").innerHTML=fruits;
+
+// for loop:
+    
+//  for(let i = 1;i<=5; i++){
+//     console.log('iteration number'+i)
+//  }
+
+// while loop:
+ 
+//  i=1
+//  while(i<=5){
+//    console.log('iteration number'+i)
+//    i++
+
+//  }
+
+// do -while loop:
+
+let i = 1
+
+
+do {
+console.log('iteration number' +i)
+   i++
+} while (i <= 6)
